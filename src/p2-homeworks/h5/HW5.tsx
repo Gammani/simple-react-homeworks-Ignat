ef44e7f92186ca8b0fe85bd38614711aae297fe2
Oreda-Z <shtucer31@gmail.com>
2022-02-15 13:49:03 +0300

    feat: completed hometask

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import Header from './Header'
-import Routes from './Routes'
+import Routing from './Routing'
+import {HashRouter} from "react-router-dom";
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
+            <HashRouter>
 
             <Header/>
 
-            <Routes/>
+            <Routing/>
 
-            {/*</HashRouter>*/}
+            </HashRouter>
         </div>
     )
 }
